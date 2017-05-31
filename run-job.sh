a525985962a5cd9e9715c514d31313ea61bf0eee
blugenie <andre.falgin@gmail.com>
2017-06-01 01:58:50 +0200

    Add files via upload
    improved run-script

--- run-job.sh
+++ run-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-time hadoop jar contrib/streaming/hadoop-*streaming*.jar \
+time hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.3*.jar \
 -file /home/hduser/mapper.py    -mapper /home/hduser/mapper.py \
 -file /home/hduser/reducer.py   -reducer /home/hduser/reducer.py \
--input /user/million/all.txt* -output /user/million/outputresults |& tee runlog.txt
+-input /user/million/all.txt -output /user/million/outputresults2 |& tee runlog.txt
