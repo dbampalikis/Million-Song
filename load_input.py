12c711d4839e5b0fb49026cafa75e9677083d704
siripersson <siri.persson1@gmail.com>
2017-06-03 16:36:34 +0200

    Update load_input.py

--- load_input.py
+++ load_input.py
@@ -1,17 +1,21 @@
-import os
-import hdf5_getters
+# File to load the input to the MapReduce
+
+import os # Module providing a portable way of using operating system dependent functionality
+import hdf5_getters 
 import sys
 
-
+# Root directory
 rootdir = '../MillionSongSubset/data/A/A'
+
+# Go through the subdirectoies, directories and files in the root directory
 for subdir, dirs, files in os.walk(rootdir):
    for file in files:
       if file.endswith(".h5"):
+         # Open the file
          h5 = hdf5_getters.open_h5_file_read(subdir+"/"+file)
+         # Get similiar artists
          similar = hdf5_getters.get_similar_artists(h5)
          for i in range (0,99):
-         	#f.write(similar[i]+"\n")
-         	#print(similar[i])
-         	#print("test")
+            # Write to standard out
             sys.stdout.write(similar[i]+"\n")
          h5.close()
